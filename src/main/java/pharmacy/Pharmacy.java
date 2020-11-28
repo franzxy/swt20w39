57cbb3f3735fbe33a563ff4b17e9ddb7032096f6
timontrettin <timon.trettin@icloud.com>
2020-11-28 21:36:55 +0100

    working prototype, please expand on this

--- src/main/java/pharmacy/Pharmacy.java
+++ src/main/java/pharmacy/Pharmacy.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package pharmacy;
 
 import org.salespointframework.EnableSalespoint;
@@ -20,7 +5,8 @@ import org.salespointframework.SalespointSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableSalespoint
 public class Pharmacy {
@@ -30,19 +16,26 @@ public class Pharmacy {
 	}
 
 	@Configuration
+	static class PharmacyWebConfiguration implements WebMvcConfigurer {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("/login").setViewName("login");
+			registry.addViewController("/").setViewName("index");
+		}
+	}
+
+	@Configuration
 	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable();  // for lab purposes, that's ok!
-			http.authorizeRequests().antMatchers("/**").permitAll().and()
-					.formLogin().loginProcessingUrl("/login").and()
-					.logout().logoutUrl("/logout").logoutSuccessUrl("/");
-		}
-	}
 
-	@GetMapping("/")
-	public String index() {
-		return "inventory";
+			http.csrf().disable();
+
+			http.authorizeRequests().antMatchers("/**").permitAll().and().
+				formLogin().loginPage("/login").loginProcessingUrl("/login").and().
+				logout().logoutUrl("/logout").logoutSuccessUrl("/");
+		}
 	}
 }
