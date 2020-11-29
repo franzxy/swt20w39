ed45e6be56637cfca55ec1d8df02e1ff8ad8308e
timontrettin <timon.trettin@icloud.com>
2020-11-29 16:49:53 +0100

    useres baseline

---
+++ src/main/java/pharmacy/users/RegistrationForm.java
@@ -0,0 +1,47 @@
+package pharmacy.users;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Pattern;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.salespointframework.useraccount.Password.UnencryptedPassword;
+
+class RegistrationForm {
+	
+	
+	@NotEmpty(message = "{RegistrationForm.name.NotEmpty}")
+	private final String name;
+
+	@NotEmpty(message = "{RegistrationForm.password.NotEmpty}")
+	@NotEmpty(message = "{PasswordForm.newPassword.NotEmpty}")
+	@Size(min = 8, max = 128, message = "{PasswordForm.newPassword.Size}")
+	@Pattern(regexp="^(?=.*[a-z]).+$", message = "{PasswordForm.newPassword.Lower}")
+	@Pattern(regexp="^(?=.*[A-Z]).+$", message = "{PasswordForm.newPassword.Upper}")
+	@Pattern(regexp="^(?=.*[-+_!@#$%^&*.,?]).+$", message = "{PasswordForm.newPassword.Special}")
+	@Pattern(regexp="^[\\S]+$", message = "{PasswordForm.newPassword.Space}")
+	private final String password;
+
+	@NotEmpty(message = "{RegistrationForm.address.NotEmpty}")
+	private final String address;
+	
+
+	public RegistrationForm(String name, String password, String address) {
+		this.name = name;
+		this.password = password;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+}
---
+++ src/main/java/pharmacy/users/UserController.java
@@ -0,0 +1,69 @@
+package pharmacy.users;
+
+import javax.validation.Valid;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+class UserController {
+
+	private final UserManagement userManagement;
+
+	UserController(UserManagement userManagement) {
+
+		Assert.notNull(userManagement, "UserManagement must not be null!");
+
+		this.userManagement = userManagement;
+	}
+
+	@PostMapping("/password")
+	String changePassword(@Valid @ModelAttribute("form")PasswordForm form, Errors result) {
+		if (result.hasErrors()) {
+			return "password";
+		}
+		userManagement.changePassword(form);
+		
+		return "redirect:/";
+	}
+
+	@GetMapping("/password")
+	String changePassword(Model model, PasswordForm form) {
+		model.addAttribute("form", form);
+		return "password";
+	}
+
+    @PostMapping("/register")
+	String registerNew(@Valid @ModelAttribute("form")RegistrationForm form, Errors result) {
+
+		if (result.hasErrors()) {
+			return "register";
+		}
+		
+		userManagement.createUser(form);
+
+		return "redirect:/";
+	}
+
+	@GetMapping("/register")
+	String register(Model model, RegistrationForm form) {
+		model.addAttribute("form", form);
+		return "register";
+	}
+
+	@GetMapping("/users")
+	@PreAuthorize("hasRole('BOSS')")
+	String users(Model model) {
+
+		model.addAttribute("userList", userManagement.findAll());
+
+		return "users";
+	}
+}
