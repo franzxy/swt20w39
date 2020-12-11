fb6962a9730c95b29dcdb986003bdee36333d5bb
Falk Natkowski <falk.natkowski@web.de>
2020-12-11 13:38:50 +0100

    Self service counters, doctors storage scanner in CounterController

--- src/main/java/pharmacy/counters/CounterController.java
+++ src/main/java/pharmacy/counters/CounterController.java
@@ -1,5 +1,6 @@
 package pharmacy.counters;
 
+import org.javamoney.moneta.Money;
 import org.salespointframework.order.Cart;
 import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderManagement;
@@ -15,8 +16,14 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import pharmacy.catalog.Medicine;
+import pharmacy.catalog.MedicineCatalog;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Optional;
+
+import static org.salespointframework.core.Currencies.EURO;
 
 @Controller
 public class CounterController {
@@ -24,5 +31,29 @@ public class CounterController {
 	@GetMapping("/counter")
 	String counter() {
 		return "counter";
+	}
+
+	@GetMapping("/selfservicecounter")
+	@PreAuthorize("hasRole('BOSS')")
+	public String selfServiceCounter(MedicineCatalog catalog, Model model) {
+
+		model.addAttribute("catalog", catalog.findByPresType(Medicine.PrescriptionType.WITHOUTPRES));
+
+		return "selfservicecounter";
+	}
+
+	@GetMapping("/doctorsstorage")
+	//@PreAuthorize("hasRole('BOSS')")
+	public String doctorsStorage(Model model) {
+		model.addAttribute("DoctorsStorageForm", new DoctorsStorageForm());
+
+		return "doctorsstorage";
+	}
+
+	@PostMapping("/doctorsstorage")
+	public String submitDoctorsStorage(@ModelAttribute DoctorsStorageForm form, Model model) {
+		model.addAttribute("DoctorsStorageForm", form);
+		return "redirect:/medicine/" + form.getID();
+
 	}
 }
---
+++ src/main/java/pharmacy/counters/DoctorsStorageForm.java
@@ -0,0 +1,13 @@
+package pharmacy.counters;
+
+public class DoctorsStorageForm {
+	private String id;
+
+	public String getID() {
+		return id;
+	}
+
+	public void setID(String s) {
+		this.id = s;
+	}
+}
