a29f5a8138cbc6cf9fd160b1088917ec8701c6b3
Lukas-Luger <lukasluger@gmx.de>
2020-12-13 20:05:51 +0100

    removed testvalues, maybe remove this class completely in the future

--- src/main/java/pharmacy/finances/FinanceDataInitializer.java
+++ src/main/java/pharmacy/finances/FinanceDataInitializer.java
@@ -1,22 +1,15 @@
 package pharmacy.finances;
 
-import javax.annotation.PostConstruct;
-
 import org.javamoney.moneta.Money;
 import org.salespointframework.accountancy.Accountancy;
-import org.salespointframework.accountancy.ProductPaymentEntry;
+import org.salespointframework.accountancy.AccountancyEntry;
 import org.salespointframework.core.DataInitializer;
-import org.salespointframework.order.OrderManagement;
-import org.salespointframework.payment.Cash;
-import org.salespointframework.time.BusinessTime;
-import org.salespointframework.useraccount.Password;
-import org.salespointframework.useraccount.Role;
-import org.salespointframework.useraccount.UserAccount;
-import org.salespointframework.useraccount.UserAccountManagement;
-
 import org.salespointframework.order.Order;
-import org.springframework.beans.factory.annotation.Value;
+import org.salespointframework.order.OrderManagement;
+import org.salespointframework.time.BusinessTime;
 import org.springframework.stereotype.Component;
+
+import pharmacy.users.UserManagement;
 
 
 
@@ -25,13 +18,11 @@ import org.springframework.stereotype.Component;
 public class FinanceDataInitializer  implements DataInitializer{
 	
 	private final OrderManagement<Order> orderManagement;
-	private final UserAccountManagement um;
+	private final UserManagement um;
 	private final Accountancy acc;
 	private final BusinessTime time;
 	
-	
-	private Filter filter;
-	public FinanceDataInitializer( OrderManagement<Order> orderManagement, UserAccountManagement um, Accountancy acc, BusinessTime time) {
+	public FinanceDataInitializer( OrderManagement<Order> orderManagement, UserManagement um, Accountancy acc, BusinessTime time) {
 		this.acc = acc;
 		this.orderManagement = orderManagement;
 		this.um = um;
@@ -42,17 +33,9 @@ public class FinanceDataInitializer  implements DataInitializer{
 	public void initialize() {
 		
 		// TODO Auto-generated method stub
-		UserAccount u1 = this.um.create("Hans", Password.UnencryptedPassword.of("123"), Role.of("CUSTOMER"));
+		this.acc.add(new AccountancyEntry(Money.of(30, "EUR"), "Default test"));
 		
-		this.um.save(u1);
-		this.um.enable(u1.getId());
-		
-		Order order= new Order(u1,Cash.CASH);
-		
-		order.addChargeLine(Money.of(10, "EUR"), "MeDiKaMeNt");
-		
-		this.orderManagement.payOrder(order);
-		this.orderManagement.save(order);
+		//System.out.println(this.acc.);
 	}
 
 }
