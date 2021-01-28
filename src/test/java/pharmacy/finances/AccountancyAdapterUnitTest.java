72c40cfe720c5aa7f6a68d10416a0b550b5cafb3
Lukas-Luger <lukasluger@gmx.de>
2021-01-28 12:51:41 +0100

    createExamples now calls autopay because scheduled does not work in test

--- src/test/java/pharmacy/finances/AccountancyAdapterUnitTest.java
+++ src/test/java/pharmacy/finances/AccountancyAdapterUnitTest.java
@@ -2,7 +2,9 @@ package pharmacy.finances;
 
 import javax.transaction.Transactional;
 
+import org.aspectj.lang.annotation.Before;
 import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.salespointframework.accountancy.Accountancy;
 import org.salespointframework.order.Order;
@@ -15,32 +17,47 @@ import org.salespointframework.useraccount.UserAccount;
 import org.salespointframework.useraccount.UserAccountManagement;
 import org.salespointframework.useraccount.Password.UnencryptedPassword;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.salespointframework.accountancy.AccountancyEntry;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.time.Duration;
 
+import pharmacy.AbstractIntegrationTests;
+import pharmacy.finances.FilterForm.Filter;
 import pharmacy.user.UserManagement;
 
 @Transactional
-public class AccountancyAdapterUnitTest {
+public class AccountancyAdapterUnitTest extends AbstractIntegrationTests{
 
     @Autowired
     private Accountancy accountancy;
 
     @Autowired
-    private UserAccountManagement userAccountManagement;
+    private UserManagement userManagement;
 
     @Autowired
     private BusinessTime businessTime;
 
     @Autowired
     private OrderManagement<Order> orderManagement;
-
     @Autowired
     private AccountancyAdapter accountancyAdapter;
 
-    
+   @BeforeEach
+   void setup(){
+       //this.accountancyAdapter = new AccountancyAdapter(this.accountancy,this.userManagement,this.businessTime,
+       //this.orderManagement);
+   }
     @Test   
     void nullTest(){
         assertThatExceptionOfType(NullPointerException.class) //
@@ -55,6 +72,47 @@ public class AccountancyAdapterUnitTest {
                 .isThrownBy(() -> accountancyAdapter.get(null));
         
     }
+    Optional<AccountancyEntry> contains(HashMap<AccountancyEntry, LocalDate> map,String descriptionpart){
+        for(Map.Entry<AccountancyEntry, LocalDate> entry : map.entrySet()){
+            if(entry.getKey().getDescription().contains(descriptionpart)){
+                return Optional.of(entry.getKey());
+            }
+        }
+        return Optional.empty();
+    }
+    @Test
+    void fixTest(){
+        Fixcosts fix = new Fixcosts();
+        
+        fix.setRent(11);
+        fix.setElectricity(12);
+        fix.setHeating(13);
+        fix.setWater(14);
+
+        this.accountancyAdapter.setFix(fix);
+
+        Fixcosts fix2 = this.accountancyAdapter.getFix();
+
+        assertEquals(fix2, fix);
+
+        this.businessTime.forward(Duration.ofDays(32));
+
+        this.accountancyAdapter.createExamples();
+
+        FilterForm f = new FilterForm();
+
+        f.setFilter(Filter.ALLE);
+
+        HashMap<AccountancyEntry, LocalDate> map = this.accountancyAdapter.filter(f);
+       
+        
+        assertTrue( contains(map, "Stro").isPresent());
+        assertTrue( contains(map, "Miet").isPresent());
+        assertTrue( contains(map, "Wasse").isPresent());
+        assertTrue( contains(map, "Heizkoste").isPresent());
+
+    }
+
     
     
 }
