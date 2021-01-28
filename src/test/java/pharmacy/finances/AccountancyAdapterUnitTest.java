49bf6d40a70a4ec6e1063a9a0a769d89cbd99f65
Lukas-Luger <lukasluger@gmx.de>
2021-01-28 13:31:21 +0100

    more tests

--- src/test/java/pharmacy/finances/AccountancyAdapterUnitTest.java
+++ src/test/java/pharmacy/finances/AccountancyAdapterUnitTest.java
@@ -4,6 +4,7 @@ import javax.transaction.Transactional;
 
 import org.aspectj.lang.annotation.Before;
 import org.javamoney.moneta.Money;
+import org.javatuples.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.salespointframework.accountancy.Accountancy;
@@ -43,7 +44,7 @@ public class AccountancyAdapterUnitTest extends AbstractIntegrationTests{
     private Accountancy accountancy;
 
     @Autowired
-    private UserManagement userManagement;
+    private UserAccountManagement userAccountManagement;
 
     @Autowired
     private BusinessTime businessTime;
@@ -53,11 +54,6 @@ public class AccountancyAdapterUnitTest extends AbstractIntegrationTests{
     @Autowired
     private AccountancyAdapter accountancyAdapter;
 
-   @BeforeEach
-   void setup(){
-       //this.accountancyAdapter = new AccountancyAdapter(this.accountancy,this.userManagement,this.businessTime,
-       //this.orderManagement);
-   }
     @Test   
     void nullTest(){
         assertThatExceptionOfType(IllegalArgumentException.class) //
@@ -81,7 +77,7 @@ public class AccountancyAdapterUnitTest extends AbstractIntegrationTests{
         return Optional.empty();
     }
     @Test
-    void fixTest(){
+    void fixFilterGetTest(){
         Fixcosts fix = new Fixcosts();
         
         fix.setRent(11);
@@ -106,12 +102,89 @@ public class AccountancyAdapterUnitTest extends AbstractIntegrationTests{
         HashMap<AccountancyEntry, LocalDate> map = this.accountancyAdapter.filter(f);
        
         
+        
         assertTrue( contains(map, "Stro").isPresent());
         assertTrue( contains(map, "Miet").isPresent());
         assertTrue( contains(map, "Wasse").isPresent());
         assertTrue( contains(map, "Heizkoste").isPresent());
+        
+        AccountancyEntry example = contains(map, "Test").get();
+
+        Pair<AccountancyEntry, LocalDate> expair = this.accountancyAdapter.get(example.getId());
+        assertTrue(expair.getValue0().equals(example));
+
+        //Filter test
+
+        f.setFilter(Filter.HEIZ);
+
+        map = this.accountancyAdapter.filter(f);
+       
+        
+        assertTrue( contains(map, "Stro").isEmpty());
+        assertTrue( contains(map, "Miet").isEmpty());
+        assertTrue( contains(map, "Wasse").isEmpty());
+        assertTrue( contains(map, "Heizkoste").isPresent());
+
+        f.setFilter(Filter.MIETE);
+
+        map = this.accountancyAdapter.filter(f);
+       
+        
+        assertTrue( contains(map, "Stro").isEmpty());
+        assertTrue( contains(map, "Miet").isPresent());
+        assertTrue( contains(map, "Wasse").isEmpty());
+        assertTrue( contains(map, "Heizkoste").isEmpty());
+
+        f.setFilter(Filter.OBEST);
+
+        map = this.accountancyAdapter.filter(f);
+       
+        
+        assertTrue( contains(map, "Stro").isEmpty());
+        assertTrue( contains(map, "Miet").isEmpty());
+        assertTrue( contains(map, "Wasse").isEmpty());
+        assertTrue( contains(map, "Heizkoste").isEmpty());
+
+        f.setFilter(Filter.STROM);
+
+        map = this.accountancyAdapter.filter(f);
+       
+        
+        assertTrue( contains(map, "Stro").isPresent());
+        assertTrue( contains(map, "Miet").isEmpty());
+        assertTrue( contains(map, "Wasse").isEmpty());
+        assertTrue( contains(map, "Heizkoste").isEmpty());
+
+        f.setFilter(Filter.WASSER);
+
+        map = this.accountancyAdapter.filter(f);
+       
+        
+        assertTrue( contains(map, "Stro").isEmpty());
+        assertTrue( contains(map, "Miet").isEmpty());
+        assertTrue( contains(map, "Wasse").isPresent());
+        assertTrue( contains(map, "Heizkoste").isEmpty());
+
+        assertTrue(this.accountancyAdapter.getRevenue().isEqualTo(Money.of(43, "EUR")));
+        assertTrue(this.accountancyAdapter.getBalance().isEqualTo(Money.of(-3007, "EUR")));
+        assertTrue(this.accountancyAdapter.getExpenses().isEqualTo(Money.of(-3050, "EUR")));
+
+        UserAccount apo = this.userAccountManagement.findByUsername("apo").get();
+
+        map = this.accountancyAdapter.findByUserAccount(apo);
+
+        assertTrue( contains(map, "Rechnung Nr.").isPresent());
+
+        AccountancyEntry orderentry = contains(map, "Rechnung Nr.").get();
+
+        assertNotNull(this.accountancyAdapter.getOrder(orderentry.getId()));
+        
+       
+
+
 
     }
+   
 
     
     
