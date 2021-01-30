57d73cea0ed81108cca5db2d6f01823bb62e24ef
Lukas-Luger <lukasluger@gmx.de>
2021-01-30 13:14:13 +0100

    javadoc and code optimization

--- src/main/java/pharmacy/inventory/InventoryController.java
+++ src/main/java/pharmacy/inventory/InventoryController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.salespointframework.accountancy.Accountancy;
+import org.salespointframework.accountancy.AccountancyEntry;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
 import org.salespointframework.order.Order;
@@ -30,6 +32,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import pharmacy.catalog.Medicine;
 import pharmacy.catalog.MedicineCatalog;
 
+/**
+ * Ein Controller für:
+ * <ul>
+ * 	<li>Warenübersicht</li>
+ * <li>Hinzufügen, Bearbeiten und Löschen von Medikamenten</li>
+ * <li>Automatisches nachbestellen</li>
+ * </ul>
+ * @author Lukas Luger
+ */
 @Controller
 @EnableScheduling
 class InventoryController {
@@ -41,10 +52,7 @@ class InventoryController {
 	private MedicineCatalog medicineCatalog;
 
 	@Autowired
-	private final UserAccountManagement userAccount;
-
-	@Autowired
-	private final OrderManagement<Order> orderManagement;
+	private final Accountancy accountancy;
 
 	@Autowired
 	private BusinessTime time;
@@ -53,53 +61,77 @@ class InventoryController {
 
 	@Autowired
 	private Map<String, Integer> waitlist;
-
 	
+
+	/**
+	 * Initialisiert den Controller.
+	 * @param inventory
+	 * @param medicineCatalog
+	 * @param userAccount
+	 * @param orderManagement
+	 * @param time
+	 * @param waitlist
+	 */
 	InventoryController(UniqueInventory<UniqueInventoryItem> inventory, MedicineCatalog medicineCatalog, 
-			UserAccountManagement userAccount, OrderManagement<Order> orderManagement, BusinessTime time, 
-			Map<String, Integer> waitlist) {
+			Accountancy accountancy, BusinessTime time, Map<String, Integer> waitlist) {
 
 		Assert.notNull(inventory, "Inventory must not be null!");
 		Assert.notNull(medicineCatalog, "MedicineCatalog must not be null!");
-		Assert.notNull(userAccount, "UserAccountManagement must not be null!");
-		Assert.notNull(orderManagement, "OrderManagement must not be null!");
+		Assert.notNull(accountancy, "OrderManagement must not be null!");
 		Assert.notNull(time, "BusinessTime must not be null!");
 		Assert.notNull(waitlist, "Waitlist must not be null!");
 
 		this.inventory = inventory;
 		this.medicineCatalog=medicineCatalog;
-		this.userAccount=userAccount;
-		this.orderManagement=orderManagement;
+		this.accountancy=accountancy;
 		this.waitlist=waitlist;
 		this.time=time;
 		this.now=LocalDateTime.now();
 		
 	}
-	
+	/**
+	 * Geht alle Medikamente im Inventar durch und vergleicht ist mit soll Wert des Vorhandenseins.
+	 * Zusätzlich wird dies mit der "waitlist" abgeglichen und letzendlich {@link #restock(int, String)} aufgreufen.
+	 * Zusätzlich wird die
+	 * Die "waitlist" wird geleert.
+	 */
 	private void autorestock(){
 
 		this.inventory.findAll().forEach(item->{
 
-			if(!item.hasSufficientQuantity(Quantity.of(((Medicine)item.getProduct()).getQuantity()))){
+			int anz = 0;
 
-				int anz = ((Medicine)item.getProduct()).getQuantity() - item.getQuantity().getAmount().intValue(); 
+			String id = item.getId().getIdentifier();
 
-				this.restock(anz, item.getId().getIdentifier());
+			Medicine med = (Medicine)item.getProduct();
+
+			if(!item.hasSufficientQuantity(Quantity.of(med.getQuantity()))){
+
+				anz = med.getQuantity() - item.getQuantity().getAmount().intValue();
+			
+			}
+
+			if(this.waitlist.containsKey(id)){
+
+				anz += this.waitlist.get(id);
 
 			}
 
+			if(anz!=0){
+
+				this.restock(anz, id);
+
+			}	
+
 		});
-
-		this.waitlist.forEach((k,v) -> {
-
-			restock(v, k);
-
-		});
-
-		this.waitlist=new HashMap<String, Integer>();
+		this.waitlist = new HashMap<String, Integer>();
 
 	}
-
+	/**
+	 * Findet ein Item aus dem Inventar mithilfe eines Strings als id.
+	 * @param id
+	 * @return {@link UniqueInventoryItem}
+	 */
 	private UniqueInventoryItem findById(String id){
 
 		return this.inventory.findAll().filter(item ->{
@@ -109,7 +141,9 @@ class InventoryController {
 		}).stream().findFirst().get();
 
 	}
-
+	/**
+	 * Falls ende des Tages: rufe {@link #autorestock()} auf.
+	 */
 	@Scheduled(fixedRate = 500)
 	protected void endofdaydetector(){
 
@@ -127,7 +161,12 @@ class InventoryController {
 		this.now=time.getTime();
 
 	}
-
+	/**
+	 * Erstellt einen {@link AccountancyEntry} für die x malige Nachbestellung des übergebenen Medikaments.
+	 * Gleichzeitig wird das Medikament aufgestockt.
+	 * @param anz
+	 * @param id
+	 */
 	private void restock(int anz, String id){
 
 		UniqueInventoryItem item = this.findById(id);
@@ -136,17 +175,19 @@ class InventoryController {
 
 		inventory.save(item.increaseQuantity(Quantity.of(anz)));
 		
-		Order o1 = new Order(this.userAccount.findByUsername("apo").get());
+		
+		AccountancyEntry order = new AccountancyEntry(med.getPurchaseprice().multiply(-1*anz),
+			"Nachbestellung von "+anz+" mal "+med.getName());
 
-		o1.addChargeLine(med.getPurchaseprice().multiply(-1F*anz), "Nachbestellung von: "+anz+"x "+med.getName());
-		o1.setPaymentMethod(Cash.CASH);
-
-		this.orderManagement.save(o1);
-		this.orderManagement.payOrder(o1);
-		this.orderManagement.completeOrder(o1);
+		this.accountancy.add(order);
 	
 	}
-	
+
+	/**
+	 * Fügt notwendige Attribute zum Model hinzu, wenn die Inventarübersicht aufgerufen wird.
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@GetMapping("/inventory")
 	@PreAuthorize("hasRole('BOSS')")
 	String inventory(Model model) {
@@ -159,6 +200,12 @@ class InventoryController {
 	
 	}
 
+	/**
+	 * Siehe {@link #inventory}
+	 * (Existiert falls jemals "/inventory" mit "post" aufgerufen wird)
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@PostMapping("/inventory")
 	@PreAuthorize("hasRole('BOSS')")
 	String filtern( Model model) {
@@ -170,7 +217,12 @@ class InventoryController {
 		return "inventory";
 
 	}
-
+	/**
+	 * Falls "/addmed" aufgerufen wird, wird direkt zu "/inventory" ungeleitet, da addmed nicht existiert.
+	 * 
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@GetMapping("/addmed")
 	@PreAuthorize("hasRole('BOSS')")
 	String premed(Model model) {
@@ -178,7 +230,17 @@ class InventoryController {
 		return "redirect:/inventory";
 
 	}
-
+	/**
+	 * Wenn "/addmed" mit "post" aufgerufen wird, wird die übergebene {@link MedicineForm} ausgewertet.
+	 * Das heißt, es wird geprüft ob ein Medikament erstellt oder nur verändert wurde mittels der id des Formulars.
+	 * Falls das Medikament existiert wird dieses Gelöscht und als neues Medikament hinzugefügt.
+	 * Falls es ein neues ist wird es nur Hinzugefügt. Das Formular wird zusätzlich auf fehler überprüft.
+	 * (Das Hinzufügen und entfernen erfolgt sowohl im Inventar als auch im Katalog)
+	 * @param formular
+	 * @param result
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	//When adding, a new medicine is created, the target quantity is defined in the medicine itself, 
 	//but there is none in the inventory yet.
 	//Since the target quantity is set in the medicine, the autorestock method will stop there and automatically 
@@ -222,7 +284,11 @@ class InventoryController {
 		return "redirect:/inventory";
 	}
 
-
+	/**
+	 * Falls "/increase" aufgerufen wird, wird direkt zu "/inventory" ungeleitet, da increase nicht existiert.
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	//Increasing a Medicine Quantity, will mean that the Medicine is added to a waitlist that will be processed by 
 	//autorestock.
 	//after that the waitlist is cleared and the Medicine will be orderd by its original Quantity
@@ -233,7 +299,15 @@ class InventoryController {
 		return "redirect:/inventory";
 
 	}
-
+	/**
+	 * Diese Methode wird bei dem clicken des "+1" Buttons im Inventar ausgelößt.
+	 * Als Identifikator wird die {@link MedicineForm} genutzt mittels id.
+	 * Fügt das Medikament in Form einer String id mit +1 zur "waitlist" hinzu.
+	 * Leitet zu "/inventory" um.
+	 * @param formular
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@PostMapping("/increase")
 	@PreAuthorize("hasRole('BOSS')")
 	String inreaseQuantity(@ModelAttribute MedicineForm formular, Model model) {
@@ -255,12 +329,27 @@ class InventoryController {
 		return "redirect:/inventory";
 
 	}
-	
+	/**
+	 * Falls "/delete" aufgerufen wird, wird direkt zu "/inventory" ungeleitet, da delete nicht existiert.
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@GetMapping("/delete")
 	@PreAuthorize("hasRole('BOSS')")
 	String predelete(Model model) {
 		return "redirect:/inventory";
 	}
+
+	/**
+	 * Diese Methode wird bei dem clicken des Löschen Buttons im Inventar ausgelößt.
+	 * Als Identifikator wird die {@link MedicineForm} genutzt mittels id.
+	 * Löscht das Medikament aus dem Inventar und dem Katalog.
+	 * Fügt das neue Inventar zum Model hinzu.
+	 * Leitet zu "/inventory" um.
+	 * @param formular
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@PostMapping("/delete")
 	@PreAuthorize("hasRole('BOSS')")
 	String delete(@ModelAttribute MedicineForm formular, Model model) {
@@ -276,7 +365,11 @@ class InventoryController {
 		return "redirect:/inventory";
 	
 	}
-
+	/**
+	 * Falls "/meddetail" aufgerufen wird, wird zu "/meddetail" weitergeleitet.
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@GetMapping("/meddetail")
 	@PreAuthorize("hasRole('BOSS')")
 	String predetails(Model model) {
@@ -284,7 +377,15 @@ class InventoryController {
 		return "meddetail";
 
 	}
-
+	/**
+	 * Ist für die Editierungsseite eine Medikaments zuständig.
+	 * Falls die Id im übergebenen Formular nicht leer ist wird das Formular mit den passenden Attributen
+	 * der dazugehörigen Medizin gefüllt und zum Model hinzugefügt.
+	 * Falls die Id jedoch leer ist, wird ein Leeres Formular zum Model hinzugefügt.
+	 * @param formular
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@PostMapping("/meddetail")
 	@PreAuthorize("hasRole('BOSS')")
 	String details(@ModelAttribute MedicineForm formular, Model model) {
@@ -316,7 +417,11 @@ class InventoryController {
 		return "meddetail";
 
 	}
-
+	/**
+	 * Falls Abgebrochen wird, wird das Formular zur Sicherheit hier geleert.
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
 	@GetMapping("/discard")
 	@PreAuthorize("hasRole('BOSS')")
 	String flush(Model model) {
