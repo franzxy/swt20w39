efe7ff80959f58e6cdeb99d74ad9205b064caaed
timontrettin <timon.trettin@icloud.com>
2021-01-31 16:20:10 +0100

    javadoc time fixed inventory font color fixed cart button accessible

--- src/main/java/pharmacy/time/DurationForm.java
+++ src/main/java/pharmacy/time/DurationForm.java
@@ -1,9 +1,15 @@
 package pharmacy.time;
-
+/**
+ * Formular zum überspringen der Zeit.
+ * @author Timon Trettin
+ */
 class DurationForm {
 	
 	private final Long duration;
 
+    /**
+     * Initialisiert die zu überspringende Zeit in Stunden.
+     */
 	public DurationForm(Long duration) {
 		this.duration = duration;
 	}
--- src/main/java/pharmacy/time/TimeController.java
+++ src/main/java/pharmacy/time/TimeController.java
@@ -13,15 +13,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * Ein Controller für Salespoint BusinessTime.
+ * @author Timon Trettin
+ */
 @Controller
 class TimeController {
 
 	private final BusinessTime businessTime;
 
+	/**
+	 * Initialisiert den Controller.
+	 * @param businessTime
+	 */
 	TimeController(BusinessTime businessTime) {
 		this.businessTime = businessTime;
 	}
 
+	/**
+	 * Fügt notwendige Attribute zum Model hinzu, wenn die Zeitreise aufgerufen wird.
+	 * @param model
+	 * @param durationForm
+	 * @return der Name der Ansicht
+	 */
 	@GetMapping("/time")
 	@PreAuthorize("hasRole('BOSS')")
 	String time(Model model, DurationForm durationForm) {
@@ -31,7 +45,14 @@ class TimeController {
 		
 		return "time";
 	}
-
+	/**
+	 * Wenn "/time" mit "post" aufgerufen wird, wird die übergebene {@link DurationForm} ausgewertet.
+	 * Wenn es keine Fehler gibt, wird BusinessTime um die angegebene Anzahl von Stunden vorgestellt.
+	 * @param durationForm
+	 * @param result
+	 * @param model
+	 * @return der Name der Ansicht
+	 */
     @PostMapping("/time")
 	@PreAuthorize("hasRole('BOSS')")
 	String changeTime(Model model, @Valid @ModelAttribute("durationForm")DurationForm durationForm, Errors result) {
