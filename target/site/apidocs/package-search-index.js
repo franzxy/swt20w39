adb483aef0496980bf6844862702ddd475217e92
timontrettin <timon.trettin@icloud.com>
2021-12-14 16:13:05 +0100

    Revert "adf"
    This reverts commit c337bc724c3b333e841171f846d7fbbb645e7024.

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pharmacy"},{"l":"pharmacy.catalog"},{"l":"pharmacy.finances"},{"l":"pharmacy.inventory"},{"l":"pharmacy.order"},{"l":"pharmacy.time"},{"l":"pharmacy.user"}]
